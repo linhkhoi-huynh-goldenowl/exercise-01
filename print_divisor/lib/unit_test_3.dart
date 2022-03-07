4150bb9bd76b7b50297586afd091c10d44366583
linhkhoi-huynh-goldenowl <linhkhoi.huynh.goldenowl@gmail.com>
2022-03-07 15:32:11 +0700

    Update unit test 3

--- print_divisor/lib/unit_test_3.dart
+++ print_divisor/lib/unit_test_3.dart
@@ -1,18 +1,13 @@
 import 'dart:math';
 
-bool ktNguyenTo(int n){
-  if(n < 2){
-       return false;
+bool ktNguyenTo(int n) {
+  if (n < 2) {
+    return false;
+  }
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (n % i == 0) {
+      return false;
     }
-    int count = 0;
-    for(int i = 2; i <= sqrt(n); i++){
-        if(n % i == 0){
-            count++;
-        }
-    }
-    if(count == 0){
-        return true;
-    }else{
-        return false;
-    }
+  }
+  return true;
 }
